be66e12c429ed0b43eafe6752cd923ff1af2fdcb
jobeda <jobedakhatun710@gmail.com>
2023-07-25 15:01:25 +0600

    first commit

---
+++ database/seeders/AdminDatabaseSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class AdminDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name'=>'admin',
+            'email'=>'admin@gmail.com',
+            'role'=>'1',
+            'password'=>bcrypt('12345678')
+        ]);
+    }
+}
---
+++ resources/views/backend/dashboard.blade.php
@@ -0,0 +1,1 @@
+<h1>hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii</h1>
